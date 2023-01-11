b15f95e0d69604f4eac7debb1b3e47740f0f0912
Bugado <addminplayerbr1122@gmail.com>
2023-01-11 13:52:09 -0300

    fix: Module.exports

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,6 +1,6 @@
-export default function cors(request, response, next) {
+module.exports = (request, response, next) => {
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
   next();
-}
+};
