137ca64fdbcd3c7ae5adc9ed39ec6469ec5af51a
Bugado <addminplayerbr1122@gmail.com>
2023-01-11 13:45:35 -0300

    feat: CORS and some fixes

---
+++ src/app/middlewares/cors.js
@@ -0,0 +1,6 @@
+export default function cors(request, response, next) {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ require('express-async-errors');
 
 const routes = require('./routes');
 const errorHandler = require('./app/middlewares/errorHandler');
+const cors = require('./app/middlewares/cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3333;
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
 app.use(errorHandler);
 
